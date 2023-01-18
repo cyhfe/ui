c6bf3f854a265f2a92b6a49f77734c6a2cedbc61
chenyuhao <cccchenyuhao@gmail.com>
2023-01-18 12:09:40 +0800

    useEffectOnlyOnUpdate

--- src/useEffectOnlyOnUpdate/demo.tsx
+++ src/useEffectOnlyOnUpdate/demo.tsx
@@ -1,24 +1,6 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 
-function useEffectOnlyOnUpdate(
-  callback: (dependencies: React.DependencyList | undefined) => void,
-  dependencies: React.DependencyList | undefined,
-) {
-  const didMount = useRef(false);
-  const callbackRef = useRef<typeof callback | null>(null);
-
-  useLayoutEffect(() => {
-    callbackRef.current = callback;
-  });
-
-  useEffect(() => {
-    if (didMount.current) {
-      callbackRef.current?.(dependencies);
-    } else {
-      didMount.current = true;
-    }
-  }, [dependencies]);
-}
+import useEffectOnlyOnUpdate from './index';
 
 function Demo() {
   const [toggle, setToggle] = useState(false);
--- src/useEffectOnlyOnUpdate/index.tsx
+++ src/useEffectOnlyOnUpdate/index.tsx
@@ -1,3 +1,23 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useRef } from 'react';
 
-export default () => {};
+function useEffectOnlyOnUpdate(
+  callback: (dependencies: React.DependencyList | undefined) => void,
+  dependencies: React.DependencyList | undefined,
+) {
+  const didMount = useRef(false);
+  const callbackRef = useRef<typeof callback | null>(null);
+
+  useLayoutEffect(() => {
+    callbackRef.current = callback;
+  });
+
+  useEffect(() => {
+    if (didMount.current) {
+      callbackRef.current?.(dependencies);
+    } else {
+      didMount.current = true;
+    }
+  }, [dependencies]);
+}
+
+export default useEffectOnlyOnUpdate;
