78e37ac704cbd5a6f85fe7f35620ca7d9cfca474
陈宇浩 <chenyuhao@mbp.local>
2023-04-07 02:24:16 +0800

    feat: useStableCallback

---
+++ src/useStableCallback/index.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+
+/**
+ * Converts a callback to a ref to avoid triggering re-renders when passed as a
+ * prop and exposed as a stable function to avoid executing effects when
+ * passed as a dependency.
+ */
+
+function useStableCallback<T extends (...args: any[]) => any>(
+  callback: T | null | undefined,
+) {
+  const callbackRef = React.useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  });
+
+  return React.useCallback(
+    ((...args) => {
+      return callbackRef.current && callbackRef.current(...args);
+    }) as T,
+    [],
+  );
+}
+
+export { useStableCallback };
