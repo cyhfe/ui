b6c495ce412e30a0a297702b843aaabcde971ddc
陈宇浩 <chenyuhao@mbp.local>
2023-04-07 01:35:31 +0800

    feat: useControlledState

---
+++ src/utils/useControlledState.ts
@@ -0,0 +1,30 @@
+import React, { useCallback } from 'react';
+
+function useControlledState<T = any>(
+  value: T | undefined,
+  defaultValue: T,
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [unControlledState, setUnControlledState] =
+    React.useState(defaultValue);
+  const wasControlled = value !== undefined;
+  const isControlledRef = React.useRef(wasControlled);
+
+  if (isControlledRef.current !== wasControlled) {
+    console.warn(
+      `Components should not switch from controlled to uncontrolled `,
+    );
+  }
+
+  const set: React.Dispatch<React.SetStateAction<T>> = useCallback(
+    (nextState) => {
+      if (!isControlledRef.current) {
+        setUnControlledState(nextState);
+      }
+    },
+    [],
+  );
+
+  return [isControlledRef.current ? (value as T) : unControlledState, set];
+}
+
+export { useControlledState };
