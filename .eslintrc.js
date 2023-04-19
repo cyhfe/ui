44c69ae7bbe2d4dac8d999943a3980d1bfd7e8d0
chenyuhao <cccchenyuhao@gmail.com>
2023-04-20 06:51:23 +0800

    feat: tooltip

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
       { allowShortCircuit: true },
     ],
     'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'no-use-before-define': ['error', { functions: false, classes: false }],
   },
   extends: require.resolve('@umijs/lint/dist/config/eslint'),
 };
--- src/Tooltip/index.tsx
+++ src/Tooltip/index.tsx
@@ -1,21 +1,171 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /** @jsxImportSource @emotion/react */
+import { createMachine } from '@cyhfe/state-machine/src/index';
 import { css } from '@emotion/react';
-import { useStatefulRefValue } from 'rcl/useStatefulRefValue';
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Portal from '../Portal';
+import { useStatefulRefValue } from '../useStatefulRefValue';
 
-let GlobalTooltipId = null;
+let GlobalTooltipId: number | null = null;
 let genID = 0;
 
-const MOUSE_REST_TIMEOUT = 300;
-const LEAVE_TIMEOUT = 500;
+const MOUSE_REST_TIMEOUT = 100;
+const LEAVE_TIMEOUT = 300;
+
+// function clearContextId() {
+//   GlobalTooltipId = null;
+// }
+
+enum TooltipStates {
+  Idle = 'IDLE',
+
+  Focused = 'FOCUSED',
+
+  Visible = 'VISIBLE',
+
+  LeavingVisible = 'LEAVING_VISIBLE',
+
+  Dismissed = 'DISMISSED',
+}
+
+enum TooltipEvents {
+  Blur = 'BLUR',
+  Focus = 'FOCUS',
+  MouseDown = 'MOUSE_DOWN',
+  MouseEnter = 'MOUSE_ENTER',
+  MouseLeave = 'MOUSE_LEAVE',
+  MouseMove = 'MOUSE_MOVE',
+  Rest = 'REST',
+  TimeComplete = 'TIME_COMPLETE',
+}
+
+let restTimeout: number;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+let send = (_event: TooltipEvents) => {};
+
+function startRestTimer() {
+  window.clearTimeout(restTimeout);
+  restTimeout = window.setTimeout(() => {
+    send(TooltipEvents.Rest);
+  }, MOUSE_REST_TIMEOUT);
+}
+
+function clearRestTimer() {
+  window.clearTimeout(restTimeout);
+}
+
+let leavingVisibleTimer: number;
+
+function startLeavingVisibleTimer() {
+  window.clearTimeout(leavingVisibleTimer);
+  leavingVisibleTimer = window.setTimeout(
+    () => send(TooltipEvents.TimeComplete),
+    LEAVE_TIMEOUT,
+  );
+}
+
+function clearLeavingVisibleTimer() {
+  window.clearTimeout(leavingVisibleTimer);
+}
+
+const chart = {
+  initialState: TooltipStates.Idle,
+  states: {
+    [TooltipStates.Idle]: {
+      actions: {},
+      transitions: {
+        [TooltipEvents.MouseEnter]: {
+          target: TooltipStates.Focused,
+        },
+        [TooltipEvents.Focus]: {
+          target: TooltipStates.Visible,
+        },
+      },
+    },
+    [TooltipStates.Focused]: {
+      actions: {
+        onEnter: startRestTimer,
+        onExit: clearRestTimer,
+      },
+      transitions: {
+        [TooltipEvents.MouseMove]: {
+          target: TooltipStates.Focused,
+        },
+        [TooltipEvents.MouseLeave]: {
+          target: TooltipStates.Idle,
+        },
+        [TooltipEvents.MouseDown]: {
+          target: TooltipStates.Dismissed,
+        },
+        [TooltipEvents.Blur]: {
+          target: TooltipStates.Idle,
+        },
+        [TooltipEvents.Rest]: {
+          target: TooltipStates.Visible,
+        },
+      },
+    },
+    [TooltipStates.Visible]: {
+      transitions: {
+        [TooltipEvents.Focus]: {
+          target: TooltipStates.Focused,
+        },
+        [TooltipEvents.MouseEnter]: {
+          target: TooltipStates.Focused,
+        },
+        [TooltipEvents.MouseLeave]: {
+          target: TooltipStates.LeavingVisible,
+        },
+        [TooltipEvents.Blur]: {
+          target: TooltipStates.LeavingVisible,
+        },
+        [TooltipEvents.MouseDown]: {
+          target: TooltipStates.Dismissed,
+        },
+      },
+    },
+    [TooltipStates.LeavingVisible]: {
+      actions: {
+        onEnter: startLeavingVisibleTimer,
+        onExit: () => {
+          clearLeavingVisibleTimer();
+        },
+      },
+      transitions: {
+        [TooltipEvents.MouseEnter]: {
+          target: TooltipStates.Visible,
+        },
+        [TooltipEvents.Focus]: {
+          target: TooltipStates.Visible,
+        },
+        [TooltipEvents.TimeComplete]: {
+          target: TooltipStates.Idle,
+        },
+      },
+    },
+    [TooltipStates.Dismissed]: {
+      actions: {
+        onEnter: startLeavingVisibleTimer,
+      },
+      transitions: {
+        [TooltipEvents.MouseLeave]: {
+          target: TooltipStates.Idle,
+        },
+        [TooltipEvents.Blur]: {
+          target: TooltipStates.Idle,
+        },
+      },
+    },
+  },
+};
+
+const machine = createMachine(chart);
+send = machine.send.bind(machine);
 
 function useId() {
-  const [id, setId] = useState<number | null>(null);
-  useLayoutEffect(() => {
-    setId(genID++);
-  }, []);
+  const [id] = useState(() => {
+    return genID++;
+  });
   return id;
 }
 
@@ -24,14 +174,13 @@ interface TooltipProps {
   label: string;
 }
 
-function useTooltip() {}
-
 interface TooltipPopupProps {
   label: string;
   triggerElement: HTMLElement | null;
+  isVisible: boolean;
 }
 
-function TooltipPopup({ label, triggerElement }: TooltipPopupProps) {
+function TooltipPopup({ label, triggerElement, isVisible }: TooltipPopupProps) {
   const [top, setTop] = useState(0);
   const [left, setLeft] = useState(0);
   const ownRef = useRef<HTMLDivElement | null>(null);
@@ -43,11 +192,11 @@ function TooltipPopup({ label, triggerElement }: TooltipPopupProps) {
     if (!triggerRect || !ownRect) return;
     const top = triggerRect.bottom;
     const left = triggerRect.left + triggerRect.width / 2 - ownRect.width / 2;
-    console.log(triggerRect, ownRect);
+    // console.log(triggerRect, ownRect);
     setTop(top);
     setLeft(left);
   }, [ownElement, triggerElement]);
-  return (
+  return isVisible ? (
     <Portal
       ref={ownRefCallback}
       css={css`
@@ -58,23 +207,78 @@ function TooltipPopup({ label, triggerElement }: TooltipPopupProps) {
     >
       {label}
     </Portal>
+  ) : null;
+}
+
+function isTooltipVisible(id: number | null) {
+  if (id !== GlobalTooltipId) return false;
+  return (
+    machine.state === TooltipStates.Visible ||
+    machine.state === TooltipStates.LeavingVisible
   );
 }
 
 function Tooltip({ children, label }: TooltipProps) {
-  // const id = useId();
+  const id = useId();
+  const onMouseDown = () => {
+    GlobalTooltipId = id;
+    send(TooltipEvents.MouseDown);
+  };
+  const onBlur = () => {
+    GlobalTooltipId = null;
+
+    send(TooltipEvents.Blur);
+  };
+  const onFocus = () => {
+    GlobalTooltipId = id;
+    send(TooltipEvents.Focus);
+  };
+  const onMouseEnter = () => {
+    // console.log('enter', id);
+    GlobalTooltipId = id;
+    send(TooltipEvents.MouseEnter);
+  };
+  const onMouseLeave = () => {
+    GlobalTooltipId = null;
+    send(TooltipEvents.MouseLeave);
+  };
+  const onMouseMove = () => {
+    GlobalTooltipId = id;
+    send(TooltipEvents.MouseMove);
+  };
+  const trigger = {
+    onMouseDown,
+    onBlur,
+    onFocus,
+    onMouseEnter,
+    onMouseLeave,
+    onMouseMove,
+  };
   const triggerRef = useRef<HTMLElement | null>(null);
   const [triggerElement, triggerRefCallback] = useStatefulRefValue(
     triggerRef,
     null,
   );
   const child = React.Children.only(children) as any;
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    return machine.subscribe(() => {
+      setIsVisible(isTooltipVisible(id));
+    });
+  }, [id]);
   return (
     <>
       {React.cloneElement(child, {
         ref: triggerRefCallback,
+        ...trigger,
       })}
-      <TooltipPopup label={label} triggerElement={triggerElement} />
+      <TooltipPopup
+        label={label}
+        triggerElement={triggerElement}
+        isVisible={isVisible}
+      />
     </>
   );
 }
