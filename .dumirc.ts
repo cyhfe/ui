f7d6c3c9a2489ba1359daaf5b5ea8f8001eed605
chenyuhao <cccchenyuhao@gmail.com>
2023-04-21 03:02:17 +0800

    feat: build doc

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  base: '/react-component-library',
+  publicPath: '/react-component-library',
   themeConfig: {
     name: 'rcl',
   },
