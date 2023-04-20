9a8796ec2d05f524274dab9d19e7d7a94d7c655f
chenyuhao <cccchenyuhao@gmail.com>
2023-04-21 00:12:01 +0800

    fix: useDebounce

--- src/useDebounce/index.tsx
+++ src/useDebounce/index.tsx
@@ -19,31 +19,3 @@ function useDebounce<T extends (...args: any[]) => any>(
 }
 
 export { useDebounce };
-
-// // 间隔一段时间内立即执行一次，取消后续调用
-// function throttle(cb: (...args: any[]) => any, delay: number) {
-//   let shouldWait = false;
-//   return (...args: any) => {
-//     if (!shouldWait) {
-//       cb(...args);
-//       shouldWait = true;
-//       setTimeout(() => {
-//         shouldWait = false;
-//       }, delay);
-//     }
-//   };
-// }
-
-// // 间隔一段时间执行，期间被调用重新计时
-// function debounce(cb: (...args: any[]) => any, delay: number) {
-//   let shouldWait = false;
-//   return (...args: any) => {
-//     if (!shouldWait) {
-//       cb(...args);
-//       shouldWait = true;
-//       setTimeout(() => {
-//         shouldWait = false;
-//       }, delay);
-//     }
-//   };
-// }
