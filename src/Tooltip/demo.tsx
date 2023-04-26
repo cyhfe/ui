e0df2de53fb9efcd5011a973f2adc3e4729e39bb
chenyuhao <cccchenyuhao@gmail.com>
2023-04-26 14:33:40 +0800

    fix: update

--- src/Tooltip/demo.tsx
+++ src/Tooltip/demo.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { machine, Tooltip } from '.';
 function Example() {
+  // 1
   const [state, setState] = useState(machine.state);
 
   useEffect(() => {
