d16894ea6133a60288429fe4a74909cf342cbf4b
chenyuhao <cccchenyuhao@gmail.com>
2023-11-17 10:11:48 +0800

    feat: form

--- src/Form/demo.tsx
+++ src/Form/demo.tsx
@@ -1,3 +1,82 @@
-import { Demo } from '.';
+// import * as Form from '.';
 
-export default Demo;
+// export const Demo = () => {
+//   const [loading, setLoading] = React.useState(false);
+//   const [serverErrors, setServerErrors] = React.useState<{
+//     email?: boolean;
+//     password?: boolean;
+//   }>({});
+
+//   return (
+//     <>
+//       <Form.Root
+//         className={formClass()}
+//         onClearServerErrors={() => setServerErrors({})}
+//         onSubmit={async (event) => {
+//           const form = event.currentTarget;
+//           event.preventDefault();
+
+//           const formData = new FormData(form);
+
+//           setLoading(true);
+//           await wait(500);
+//           setLoading(false);
+
+//           const errors = new Set();
+//           if (!(formData.get('email') as string).includes('@gmail.com'))
+//             errors.add('email');
+//           if (!(formData.get('password') as string).includes('#'))
+//             errors.add('password');
+
+//           if (errors.size > 0) {
+//             setServerErrors(
+//               Object.fromEntries([...errors].map((name) => [name, true])),
+//             );
+//             return;
+//           }
+
+//           window.alert(JSON.stringify(Object.fromEntries(formData), null, 2));
+//         }}
+//       >
+//         <Form.Field name="email" serverInvalid={serverErrors.email}>
+//           <Form.Label>Email</Form.Label>
+//           <Form.Control
+//             type="email"
+//             required
+//             onChange={() =>
+//               setServerErrors((prev) => ({ ...prev, email: false }))
+//             }
+//           />
+//           <Form.Message match="valueMissing" />
+//           <Form.Message match="typeMismatch" forceMatch={serverErrors.email}>
+//             Email is invalid
+//           </Form.Message>
+//         </Form.Field>
+
+//         <Form.Field name="password" serverInvalid={serverErrors.password}>
+//           <Form.Label>Password</Form.Label>
+//           <Form.Control
+//             type="password"
+//             required
+//             onChange={() =>
+//               setServerErrors((prev) => ({ ...prev, password: false }))
+//             }
+//           />
+//           <Form.Message match="valueMissing">Password is required</Form.Message>
+//           <Form.Message
+//             match={(value) => value.match(/.*[0-9]+.*/) === null}
+//             forceMatch={serverErrors.password}
+//           >
+//             Password is not complex enough
+//           </Form.Message>
+//           {serverErrors.password && <Form.Message>Woops</Form.Message>}
+//         </Form.Field>
+
+//         <Form.Submit disabled={loading}>Submit</Form.Submit>
+//         <button type="reset">Reset</button>
+//       </Form.Root>
+//     </>
+//   );
+// };
+
+// export default Demo;
--- src/Form/index.tsx
+++ src/Form/index.tsx
@@ -1,16 +1,34 @@
-function Demo() {
-  return (
-    <form
-      onSubmit={function handleSubmit(e) {
-        e.preventDefault();
-        console.log(e.currentTarget);
-      }}
-    >
-      <input name="firstname" />
-      <input name="lastname" />
-      <input type="submit" />
-    </form>
-  );
+import { useComposeRefs } from '../useComposeRefs';
+import React, { ReactNode, forwardRef } from 'react';
+
+type FormELment = HTMLFormElement;
+interface FormProps {
+  children: ReactNode;
 }
 
-export { Demo };
+const Form = forwardRef<FormELment, FormProps>(function Form(
+  formProps,
+  forwardRef,
+) {
+  const formRef = React.useRef<HTMLFormElement | null>(null);
+  const composedFormRef = useComposeRefs(forwardRef, formRef);
+  return <form {...formProps} ref={composedFormRef} />;
+});
+
+function Field() {
+  return <div></div>;
+}
+
+function Label() {}
+
+function Control() {}
+
+function Message() {}
+
+function ValidityState() {}
+
+function Submit() {}
+
+const Root = Form;
+
+export { Root, Field, Label, Control, Message, ValidityState, Submit };
